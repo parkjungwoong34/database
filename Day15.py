fb23d8c5355c8707307ca1b35068ed358bde1d78
parkjungwoong34 <https://github.com/parkjungwoong34/test04.git>
2023-02-03 11:16:31 +0900

    day15 linear list 02

--- Day15.py
+++ Day15.py
@@ -1,16 +1,24 @@
-pokemons = list[]  # 빈 배열
 
 
-def add_data(pokemon):
+
+def insert_data(idx, pokemon):
+    if idx < 0 or idx > len(pokemons):
+        print("Out of range")
+        return
+
     pokemons.append(None)
-    len_pokemons = len(pokemons)
-    # pokemons[len_pokemons - 1] = pokemon
-    pokemons[-1] = pokemon
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('이상해')
+    for i in range(len(pokemons) - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
 
-print(pokemons)
+    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
+
+if __name__ == "__main__":
+    pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "이상해"]
+    print(pokemons)
+    insert_data(2, '거북왕')
+    print(pokemons)
+    insert_data(6, '어니부기')
+    print(pokemons)
+
