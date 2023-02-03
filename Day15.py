265393b204b8933c15a841219fd7b5251975e2ea
parkjungwoong34 <https://github.com/parkjungwoong34/test04.git>
2023-02-03 10:48:23 +0900

    day15 linear list 01

---
+++ Day15.py
@@ -0,0 +1,16 @@
+pokemons = list[]  # 빈 배열
+
+
+def add_data(pokemon):
+    pokemons.append(None)
+    len_pokemons = len(pokemons)
+    # pokemons[len_pokemons - 1] = pokemon
+    pokemons[-1] = pokemon
+
+add_data('피카츄')
+add_data('라이츄')
+add_data('파이리')
+add_data('꼬부기')
+add_data('이상해')
+
+print(pokemons)
