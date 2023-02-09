8711725f98e25e9f4188e54fc8038d09496482ea
parkjungwoong34 <https://github.com/parkjungwoong34/test04.git>
2023-02-09 23:46:33 +0900

    excercise12_01

--- Day19.py
+++ Day19.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 def selection_Sort(Ary) :
 	n = len(Ary)
 	for i in range(0, n-1) :
@@ -11,19 +14,49 @@ def selection_Sort(Ary) :
 
 	return Ary
 
+def q_Sort(arr, start, end) :
+	if end <= start :
+		return
 
-ary2 = [[55, 33, 250, 44],
-		 [88,  1,  67, 23],
-		 [199,222, 38, 47],
-		 [155,145, 20, 99]]
-ary1 = []
+	low = start
+	high = end
+
+	pivot = arr[(low + high) // 2]
+	while low <= high:
+		while arr[low] < pivot :
+			low += 1
+		while arr[high] > pivot :
+			high -= 1
+		if low <= high :
+			arr[low], arr[high] = arr[high], arr[low]
+			low, high = low + 1, high - 1
+
+	mid = low
+
+	q_Sort(arr, start, mid - 1)
+	q_Sort(arr, mid, end)
+
+def quickSort(ary) :
+	q_Sort(ary, 0, len(ary) - 1)
 
 
-for i in range(len(ary2)) :
-	for k in range(len(ary2[i])) :
-		ary1.append(ary2[i][k])
+countAry = [1000, 10000, 12000, 15000]
 
-print('1차원 변경 후, 정렬 전 -->', ary1)
-ary1 = selection_Sort(ary1)
-print('1차원 변경 후, 정렬 후 -->', ary1)
-print('중앙값 --> ', ary1[len(ary1)//2])
+for count in countAry :
+	tempAry = [ random.randint(10000, 99999) for _ in range(count)]
+	selectAry = tempAry[:]
+	quickAry = tempAry[:]
+
+	print("## 데이터 수 : ", count, "개")
+	start = time.time()
+	selection_Sort(selectAry)
+	end = time.time()
+	print("	선택 정렬 --> %10.3f 초" % (end-start))
+	start = time.time()
+	quickSort(selectAry)
+	end = time.time()
+	print("	퀵 정렬  --> %10.3f 초" % (end-start))
+	print()
+
+	count *= 5
+
